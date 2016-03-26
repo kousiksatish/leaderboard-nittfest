dc547f616394b024794bf171f552f166d6782f27
Kousik Satish <kousiksatish@gmail.com>
2016-03-26 17:46:53 +0530

    Minor change to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
     	->lists('eventcluster');
     $depts = DB::table('pragyanV3_users')
     		->where('user_id', '>', '10000')
+    		->where('user_id', '<', '10013')
     		->lists('user_name');
    	$depts = array_fill_keys($depts, 0);
    	//return $depts;
